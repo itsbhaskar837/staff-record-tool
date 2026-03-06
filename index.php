5dda9aa76b9d57e1bc17a8355dd93c172ed0fb6f
Dev Kumar <dev@company.com>
2026-03-06 23:40:45 +0530

    Dev: Frontend UI created

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include 'includes/header.php'; ?>
+
+<h2>Welcome to Staff Record Tool</h2>
+
+<p>This tool allows you to manage company staff records.</p>
+
+<div class="menu">
+
+<a href="add_staff.php">Add Staff</a>
+<a href="view_staff.php">View Staff</a>
+
+</div>
+
+</div>
+</body>
+</html>
